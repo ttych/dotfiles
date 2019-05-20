f9caffb6a2901e20b16e281ef219261396b9247d
Thomas Tych <thomas.tych@gmail.com>
2019-05-20 22:33:44 +0200

    [emacs] use org mode to document configuration

--- emacs/init.el
+++ emacs/init.el
@@ -19,15 +19,6 @@
 (unless (file-exists-p savefile-dir)
   (make-directory savefile-dir))
 
-(defvar users-settings-dir (expand-file-name "users/" user-emacs-directory)
-  "This folder stores user specific setting.")
-(defvar user-settings-file
-  (expand-file-name (concat user-login-name ".el")
-                    users-settings-dir))
-(if (file-exists-p user-settings-file)
-    (load user-settings-file))
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -48,11 +39,15 @@
  '(icomplete-mode t)
  '(indent-tabs-mode t)
  '(indicate-empty-lines t)
+ '(inhibit-startup-buffer-menu t)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message nil)
  '(large-file-warning-threshold 100000000)
  '(line-number-mode t)
  '(load-prefer-newer t)
  '(max-lisp-eval-depth 2000)
  '(next-line-add-newlines nil)
+ '(org-babel-load-languages (quote ((emacs-lisp . t))))
  '(read-quoted-char-radix 16)
  '(require-final-newline t)
  '(save-place-mode t)
@@ -71,17 +66,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-; (if (fboundp 'blink-cursor-mode) (blink-cursor-mode -1))
-(setq inhibit-startup-message t
-      inhibit-startup-screen t
-      inhibit-startup-echo-area-message ""
-      inhibit-startup-buffer-menu t
-      initial-scratch-message nil)
-
 ;; (prefer-coding-system 'utf-8)
 ;; (set-default-coding-systems 'utf-8)
 ;; (set-terminal-coding-system 'utf-8)
@@ -95,5 +79,7 @@
 		 ))
 	(mapc load-it (directory-files dir nil "\\.el$"))))
 (load-directory (expand-file-name "conf/" user-emacs-directory))
+
+(org-babel-load-file (expand-file-name "myinit.org" user-emacs-directory))
 
 ;;; init.el ends here
