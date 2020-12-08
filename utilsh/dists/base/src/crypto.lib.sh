15b56004d1c1c4c0e5c3438d9743a8eb4e4b6c04
Thomas Tych <thomas.tych@gmail.com>
2020-12-08 09:49:34 +0100

    [utilsh][base] rename cert lib to crypto lib and add digest_integrity utility

---
+++ utilsh/dists/base/src/crypto.lib.sh
@@ -0,0 +1,166 @@
+#!/bin/sh
+# -*- mode: sh -*-
+
+
+
+# ###
+
+has_openssl()
+{
+    which openssl >/dev/null 2>/dev/null
+}
+
+has_sha384sum()
+{
+    which has_sha384sum >/dev/null 2>/dev/null
+}
+
+_sha()
+{
+    case `uname -s` in
+        Linux) "sha${2:-512}sum" "${1}" ;;
+        FreeBSD) "$sha${2:-512}" -r "${1}" ;;
+        *) echo >&2 "unsupported arch for _sha"
+           return 1 ;;
+    esac
+}
+
+
+
+# ### cert
+
+cert_print_j()
+{
+    if [ $# -eq 0 ]; then
+        keytool -printcert -V
+        return $?
+    fi
+
+    for cert; do
+        if [ ! -f "$cert" ]; then
+            echo >&2 "$cert is not a file"
+            continue
+        fi
+
+        if [ ! -r "$cert" ]; then
+            echo >&2 "$cert is not readable"
+            continue
+        fi
+
+        keytool -printcert -V -file $cert
+    done
+}
+
+cert_print_o()
+{
+    if [ $# -eq 0 ]; then
+        openssl x509 -text
+        return $?
+    fi
+
+    for cert; do
+        if [ ! -f "$cert" ]; then
+            echo >&2 "$cert is not a file"
+            continue
+        fi
+
+        if [ ! -r "$cert" ]; then
+            echo >&2 "$cert is not readable"
+            continue
+        fi
+
+        openssl x509 -in $cert -text
+    done
+}
+
+cert_get()
+{
+    cert_get_o "$@"
+}
+
+cert_get_o()
+{
+    openssl s_client -showcerts -connect "$1" ${2:+-servername "$2"} -prexit </dev/null
+}
+
+cert_to_pem()
+{
+    openssl x509 -outform pem ${1:+-out "$1"}
+}
+
+cert_verify()
+{
+    openssl verify ${2:+-untrusted "$2"} "$@"
+}
+
+cert_self_signed()
+{
+    cert_self_signed="${1:-cert}"
+
+    if [ -r "$cert_self_signed.pem" ]; then
+        echo >&2 "$cert_self_signed.pem already exists"
+        return 1
+    fi
+
+    openssl req -x509 -nodes -newkey rsa:4096 -keyout "$cert_self_signed.key" -out "$cert_self_signed.pem" -days "${2:-3650}"
+}
+
+
+
+# ### digest
+
+digest_integrity()
+{
+    if has_openssl; then
+        _digest_integrity_o "$@"
+        digest_integrity="$_digest_integrity_o"
+    else
+        _digest_integrity_s "$@"
+        digest_integrity="$_digest_integrity_s"
+    fi
+    echo "$digest_integrity"
+}
+
+_digest_integrity_o()
+{
+    _digest_integrity_o=
+    digest_integrity_o__file="$1"
+    digest_integrity_o__digest="${2:-512}"
+
+    if [ ! -f "$digest_integrity_o__file" ] || [ ! -r "$digest_integrity_o__file" ]; then
+        echo >&2 "could not compute integrity for \"$digest_integrity_o__file\""
+        return 1
+    fi
+
+    # cat FILENAME.js | openssl dgst -sha384 -binary | openssl base64 -A
+    _digest_integrity_o=$(openssl dgst \
+                                  -"sha$digest_integrity_o__digest" \
+                                  -binary "$digest_integrity_o__file" |
+                              openssl base64 -A)
+}
+
+_digest_integrity_s()
+{
+    _digest_integrity_s=
+    digest_integrity_s__file="$1"
+    digest_integrity_s__digest="${2:-512}"
+
+    if [ ! -f "$digest_integrity_s__file" ] || [ ! -r "$digest_integrity_s__file" ]; then
+        echo >&2 "could not compute integrity for \"$digest_integrity_s__file\""
+        return 1
+    fi
+
+    # cat FILENAME.js | openssl dgst -sha384 -binary | openssl base64 -A
+    _digest_integrity_s=$(_sha "$digest_integrity_s__file" "$digest_integrity_s__digest" |
+                              awk '{ print $1 }' | xxd -r -p | base64)
+}
+
+
+
+# ### Main
+
+case ${0##*/} in
+    cert_*)
+        ${0##*/} "$@"
+        ;;
+esac
