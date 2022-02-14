7e9b430da61bf86a6dbec72bdd15ad03db9f5265
Thomas Tych <thomas.tych@gmail.com>
2022-02-14 23:16:44 +0100

    [utilsh][base] add more cert utils

--- utilsh/dists/base/src/crypto.lib.sh
+++ utilsh/dists/base/src/crypto.lib.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # -*- mode: sh -*-
 
+# ### doc
 
+# https://web.archive.org/web/20170107050552/http://shib.kuleuven.be/docs/ssl_commands.shtml
 
 # ###
 
@@ -105,11 +107,43 @@ cert_self_signed()
     openssl req -x509 -nodes -newkey rsa:4096 -keyout "$cert_self_signed.key" -out "$cert_self_signed.pem" -days "${2:-3650}"
 }
 
-cert_is_key_file()
+cert_gen_key()
+{
+    openssl genrsa -out "$1" "${2:-4096}"
+}
+
+cert_gen_key_secured()
+{
+    openssl genrsa -aes128 -passout pass:"$3" -out "$1" "${2:-4096}"
+}
+
+cert_check_private_key()
 {
     [ -n "$1" ] || return 1
     [ -r "$1" ] || return 1
-    return 0
+
+    openssl rsa -check -in "$1"
+}
+
+cert_is_key_file()
+{
+    cert_check_private_key "$@"
+}
+
+cert_check_match_cert_key()
+{
+    modulus_a=`openssl x509 -noout -modulus -in "$1" | openssl md5`
+    modulus_b=`openssl rsa -noout -modulus -in "$2" | openssl md5`
+
+    [ "$modulus_a" = "$modulus_b" ]
+}
+
+cert_check_match_req_key()
+{
+    modulus_a=`openssl req -noout -modulus -in "$1" | openssl md5`
+    modulus_b=`openssl rsa -noout -modulus -in "$2" | openssl md5`
+
+    [ "$modulus_a" = "$modulus_b" ]
 }
 
 cert_remove_passphrase()
@@ -120,6 +154,54 @@ cert_remove_passphrase()
     fi
 
     openssl rsa -in "$1" -out "${1%.pem}.nopp.pem"
+}
+
+cert_pkcs12_to_pem()
+{
+    [ $# -ne 1 ] || {
+	echo >&2 "Usage: cert_pkcs12_to_pem <cert.p12>"
+	return 1
+    }
+    openssl pkcs12 -in "$1" -out "${1%.p12}.crt.pem" -clcerts -nokeys && \
+	openssl pkcs12 -in "$1" -out "${1%.p12}.key.pem" -nocerts -nodes
+}
+
+cert_jks_to_pkcs12()
+{
+    [ $# -ne 1 ] && return 1
+
+    # keytool \
+    # 	-importkeystore \
+    # 	-srckeystore KEYSTORE.jks \
+    # 	-srcstoretype JKS \
+    # 	-srcstorepass mysecret \
+    # 	-srcalias myalias \
+    # 	-srckeypass mykeypass \
+    # 	-destkeystore KEYSTORE.p12 \
+    # 	-deststoretype PKCS12 \
+    # 	-deststorepass mysecret \
+    # 	-destalias myalias \
+    # 	-destkeypass mykeypass \
+    # 	-noprompt
+
+    keytool \
+	-importkeystore \
+	-srckeystore "$1" \
+	-srcstoretype JKS \
+	-deststoretype PKCS12 \
+	-destkeystore "${1%.jks}.p12"
+}
+
+cert_pkcs12_to_jks()
+{
+    [ $# -ne 1 ] && return 1
+
+    keytool \
+	-importkeystore \
+	-srckeystore "$1" \
+	-srcstoretype PKCS12 \
+	-deststoretype JKS \
+	-destkeystore "${1%.p12}.jks"
 }
 
 
