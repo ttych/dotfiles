b018d17367ae6dcbbaaf295ef2b8a01de38a73a9
Thomas Tych <thomas.tych@gmail.com>
2022-02-21 08:29:56 +0100

    [emacs] remove uneeded extra loading

--- emacs/init.el
+++ emacs/init.el
@@ -31,16 +31,3 @@
   (org-babel-load-file conf-org))
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(zoom-window yasnippet-snippets yarn-mode yari yard-mode yaml-mode which-key vue-mode use-package-ensure-system-package typescript-mode tuareg terraform-mode sql-indent slim-mode scss-mode scratch ruby-electric rspec-mode robe restclient-test rainbow-mode railscasts-reloaded-theme pyenv-mode puppet-mode plantuml-mode org-bullets nov multiple-cursors minitest magit lua-mode lsp-ui ledger-mode ivy-yasnippet ini-mode iedit htmlize hl-todo haskell-mode haml-mode groovy-mode git-timemachine flyspell-correct-ivy flycheck-ledger find-file-in-project feature-mode expand-region emmet-mode eglot dockerfile-mode docbook delight dap-mode csv-mode company-box comment-dwim-2 cask-mode bundler blacken beacon auto-virtualenv auctex anzu all-the-icons ag adoc-mode ace-jump-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
