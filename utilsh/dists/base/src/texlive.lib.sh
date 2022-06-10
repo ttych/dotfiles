053c5ea8fbb75a7ea09af651c1868df59ed9f597
Thomas Tych <thomas.tych@gmail.com>
2022-06-10 16:13:38 +0200

    [utilsh][base] change texlive vars to avoid completion pollution

--- utilsh/dists/base/src/texlive.lib.sh
+++ utilsh/dists/base/src/texlive.lib.sh
@@ -7,8 +7,8 @@
 
 texlive_env()
 {
-    texlive_env__os_arch="$(uname -m)"
-    texlive_env__os_name="$(uname -s | tr '[A-Z]' '[a-z]')"
+    _texlive_env__os_arch="$(uname -m)"
+    _texlive_env__os_name="$(uname -s | tr '[A-Z]' '[a-z]')"
     DEFAULT_TEXLIVE_DIR="$HOME/local/texlive"
     TEXLIVE_DIR="${TEXLIVE_DIR:-$DEFAULT_TEXLIVE_DIR}"
     export TEXLIVE_DIR
@@ -16,16 +16,16 @@ texlive_env()
     export TEXMFHOME
 
     if [ -d "$TEXLIVE_DIR" ]; then
-        for texlive_env__d in $(ls -1d "$TEXLIVE_DIR"/ "$TEXLIVE_DIR"/[0-9]*/ 2>/dev/null | sort -r)
+        for _texlive_env__d in $(ls -1d "$TEXLIVE_DIR"/ "$TEXLIVE_DIR"/[0-9]*/ 2>/dev/null | sort -r)
         do
-            [ -x "${texlive_env__d}${TEXLIVE_INSTALLER}" ] || continue
-            [ -d "${texlive_env__d}bin" ] || continue
-            [ -d "${texlive_env__d}bin/${texlive_env__os_arch}-${texlive_env__os_name}" ] || continue
+            [ -x "${_texlive_env__d}${TEXLIVE_INSTALLER}" ] || continue
+            [ -d "${_texlive_env__d}bin" ] || continue
+            [ -d "${_texlive_env__d}bin/${_texlive_env__os_arch}-${_texlive_env__os_name}" ] || continue
 
-            path_add_pre "${texlive_env__d}bin/${texlive_env__os_arch}-${texlive_env__os_name}"
-            MANPATH="${texlive_env__d}texmf-dist/doc/man:$MANPATH"
+            path_add_pre "${_texlive_env__d}bin/${_texlive_env__os_arch}-${_texlive_env__os_name}"
+            MANPATH="${_texlive_env__d}texmf-dist/doc/man:$MANPATH"
             export MANPATH
-            INFOPATH="${texlive_env__d}texmf-dist/doc/info:$INFOPATH"
+            INFOPATH="${_texlive_env__d}texmf-dist/doc/info:$INFOPATH"
             export INFOPATH
             break
         done
