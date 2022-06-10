6d13773c0646632ded690802f87ca34a389021b2
Thomas Tych <thomas.tych@gmail.com>
2022-06-10 16:18:44 +0200

    [utilsh][base] cosmetic update

--- utilsh/dists/base/src/texlive.lib.sh
+++ utilsh/dists/base/src/texlive.lib.sh
@@ -18,14 +18,15 @@ texlive_env()
     if [ -d "$TEXLIVE_DIR" ]; then
         for _texlive_env__d in $(ls -1d "$TEXLIVE_DIR"/ "$TEXLIVE_DIR"/[0-9]*/ 2>/dev/null | sort -r)
         do
-            [ -x "${_texlive_env__d}${TEXLIVE_INSTALLER}" ] || continue
-            [ -d "${_texlive_env__d}bin" ] || continue
-            [ -d "${_texlive_env__d}bin/${_texlive_env__os_arch}-${_texlive_env__os_name}" ] || continue
+            _texlive_env__d="${_texlive_env__d%/}"
+            [ -x "${_texlive_env__d}/${TEXLIVE_INSTALLER}" ] || continue
+            [ -d "${_texlive_env__d}/bin" ] || continue
+            [ -d "${_texlive_env__d}/bin/${_texlive_env__os_arch}-${_texlive_env__os_name}" ] || continue
 
-            path_add_pre "${_texlive_env__d}bin/${_texlive_env__os_arch}-${_texlive_env__os_name}"
-            MANPATH="${_texlive_env__d}texmf-dist/doc/man:$MANPATH"
+            path_add_pre "${_texlive_env__d}/bin/${_texlive_env__os_arch}-${_texlive_env__os_name}"
+            MANPATH="${_texlive_env__d}/texmf-dist/doc/man:$MANPATH"
             export MANPATH
-            INFOPATH="${_texlive_env__d}texmf-dist/doc/info:$INFOPATH"
+            INFOPATH="${_texlive_env__d}/texmf-dist/doc/info:$INFOPATH"
             export INFOPATH
             break
         done
