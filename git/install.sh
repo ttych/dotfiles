b47746193c2534a78c81c725c46bf77dc3ae9fb3
Thomas Tych <thomas.tych@gmail.com>
2023-03-15 18:25:09 +0100

    [git] add orphan alias

--- git/install.sh
+++ git/install.sh
@@ -145,6 +145,7 @@ git config --global alias.co checkout
 git config --global alias.cob 'checkout -b'
 git config --global alias.com 'checkout master'
 git config --global alias.cor 'checkout --track'
+git config --global alias.orphan 'checkout --orphan'
 
 ## branch
 git config --global alias.br branch
