1c8f1c058afe5c6d0fa84a348a0c585e71b6bd49
Thomas Tych <thomas.tych@gmail.com>
2024-09-09 12:59:21 +0200

    [emacs] refacto emacs configuration

--- emacs/install.sh
+++ emacs/install.sh
@@ -13,3 +13,22 @@ install_snippets()
 }
 
 install_snippets
+
+#########################################
+
+conf_org='emacs-conf.org'
+
+if ! which org-babel-tangle >/dev/null 2>/dev/null; then
+    org-babel-tangle()
+    {
+	if [ -z "$1" ]; then
+	    echo >&2 "usage: org-babel-tangle <org-file>"
+	    return 1
+	fi
+	emacs --batch "$1" --funcall org-babel-tangle
+    }
+fi
+
+if [ -r "$conf_org" ]; then
+    org-babel-tangle "$conf_org"
+fi
